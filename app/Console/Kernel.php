870b4989802461a1de1e29ed8d2b21b00c25b7b0
hokeryu318 <hokeryu318>
2019-07-18 04:26:26 +0800

    0718_email_1

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
         //          ->hourly();
 //        $schedule->command('email:sender')->dailyAt('17:36');
 //        $schedule->command('email:sender')->daily();
-        $schedule->command('email:sender')->timezone('Australia/Melbourne')->at('06:20');
+        $schedule->command('email:sender')->timezone('Australia/Melbourne')->at('02:00');
 //        $schedule->command('email:sender')->everyMinute();
     }
 
