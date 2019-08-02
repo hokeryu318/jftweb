0d6b4d5fe78a728b9bcf2c2ba0e5881509403ee5
hokeryu318 <hokeryu318>
2019-08-02 15:59:47 +0800

    0802-9

--- app/Http/Controllers/ReceptionController.php
+++ app/Http/Controllers/ReceptionController.php
@@ -29,6 +29,7 @@ use Mike42\Escpos\PrintConnectors\NetworkPrintConnector;
 use Mike42\Escpos\PrintConnectors\FilePrintConnector;
 use Mike42\Escpos\Printer;
 use Mike42\Escpos\EscposImage;
+use App\Http\Controllers\print_table1;
 
 class ReceptionController extends Controller
 {
@@ -704,9 +705,59 @@ class ReceptionController extends Controller
             $printer->setJustification(Printer::JUSTIFY_LEFT);
             $printer->setTextSize(1,1);
 
-            $line = sprintf('%-20.00s %-80.00s %-200.00s %-200.00s', "Description", "Price", "Qty", "Total");
-            $printer->text($line);
-            $printer->text(".............................................\n");
+
+            $items = array(
+                new print_table1("Example item #1", "4.00"),
+                new print_table1("Another thing", "3.50"),
+                new print_table1("Something else", "1.00"),
+                new print_table1("A final item", "4.45"),
+            );
+            $subtotal = new print_table1('Subtotal', '12.95');
+            $tax = new print_table1('A local tax', '1.30');
+            $total = new print_table1('Total', '14.25', true);
+            /* Date is kept the same for testing */
+// $date = date('l jS \of F Y h:i:s A');
+            $date = "Monday 6th of April 2015 02:56:25 PM";
+
+            /* Name of shop */
+            $printer -> selectPrintMode(Printer::MODE_DOUBLE_WIDTH);
+            $printer -> text("ExampleMart Ltd.\n");
+            $printer -> selectPrintMode();
+            $printer -> text("Shop No. 42.\n");
+            $printer -> feed();
+
+            /* Title of receipt */
+            $printer -> setEmphasis(true);
+            $printer -> text("SALES INVOICE\n");
+            $printer -> setEmphasis(false);
+
+            /* Items */
+            $printer -> setJustification(Printer::JUSTIFY_LEFT);
+            $printer -> setEmphasis(true);
+            $printer -> text(new print_table1('', '$'));
+            $printer -> setEmphasis(false);
+            foreach ($items as $item) {
+                $printer -> text($item);
+            }
+            $printer -> setEmphasis(true);
+            $printer -> text($subtotal);
+            $printer -> setEmphasis(false);
+            $printer -> feed();
+
+            /* Tax and total */
+            $printer -> text($tax);
+            $printer -> selectPrintMode(Printer::MODE_DOUBLE_WIDTH);
+            $printer -> text($total);
+            $printer -> selectPrintMode();
+
+            /* Footer */
+            $printer -> feed(2);
+            $printer -> setJustification(Printer::JUSTIFY_CENTER);
+            $printer -> text("Thank you for shopping at ExampleMart\n");
+            $printer -> text("For trading hours, please visit example.com\n");
+            $printer -> feed(2);
+            $printer -> text($date . "\n");
+
 
 
             $printer->cut();
@@ -909,3 +960,5 @@ class ReceptionController extends Controller
     }
 
 }
+
+
