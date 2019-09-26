dcbe15cdd5c36a868227ad77855c76c78b9447ee
jiuhejong <hokelucpy@hotmail.com>
2019-09-26 20:19:45 +0800

    dish_order

--- resources/views/admin/category/list.blade.php
+++ resources/views/admin/category/list.blade.php
@@ -40,40 +40,15 @@
 
     function init() 
     {
-        document.addEventListener("touchstart", touchHandler, true);
-        document.addEventListener("touchmove", touchHandler, true);
-        document.addEventListener("touchend", touchHandler, true);
-        document.addEventListener("touchcancel", touchHandler, true);    
+        document.getElementById("scroll-dish").addEventListener("touchstart", touchHandler, true);
+        document.getElementById("scroll-dish").addEventListener("touchmove", touchHandler, true);
+        document.getElementById("scroll-dish").addEventListener("touchend", touchHandler, true);
+        document.getElementById("scroll-dish").addEventListener("touchcancel", touchHandler, true);
     }
     
     // $(function(){
     $(document).ready(function(){
-        function touchHandler(event)
-        {
-            var touch = event.changedTouches[0];
-
-            var simulatedEvent = document.createEvent("MouseEvent");
-            simulatedEvent.initMouseEvent({
-                    touchstart: "mousedown",
-                    touchmove: "mousemove",
-                    touchend: "mouseup"
-                }[event.type], true, true, window, 1,
-                touch.screenX, touch.screenY,
-                touch.clientX, touch.clientY, false,
-                false, false, false, 0, null);
-
-            touch.target.dispatchEvent(simulatedEvent);
-            event.preventDefault();
-        }
-
-        function init()
-        {
-            document.addEventListener("touchstart", touchHandler, true);
-            document.addEventListener("touchmove", touchHandler, true);
-            document.addEventListener("touchend", touchHandler, true);
-            document.addEventListener("touchcancel", touchHandler, true);
-        }
-        init();
+      init();
       $("#scroll-dish").sortable({
         stop: function(){
           $.map($(this).find('li'), function(el) {
