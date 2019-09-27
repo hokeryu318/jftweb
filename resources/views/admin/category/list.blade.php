746e8a4281464ceddd56c116e52c774e17f28a47
jiuhejong <hokelucpy@hotmail.com>
2019-09-27 12:36:27 +0800

    sort

--- resources/views/admin/category/list.blade.php
+++ resources/views/admin/category/list.blade.php
@@ -354,36 +354,36 @@
 <script src="https://cdn.jsdelivr.net/jquery.ui.touch-punch/0.2.3/jquery.ui.touch-punch.min.js"></script>
 
 <script>
-    // function touchHandler(event)
-    // {
-    //     var touch = event.changedTouches[0];
-    //
-    //     var simulatedEvent = document.createEvent("MouseEvent");
-    //     simulatedEvent.initMouseEvent({
-    //             touchstart: "mousedown",
-    //             touchmove: "mousemove",
-    //             touchend: "mouseup"
-    //         }[event.type], true, true, window, 1,
-    //         touch.screenX, touch.screenY,
-    //         touch.clientX, touch.clientY, false,
-    //         false, false, false, 0, null);
-    //
-    //     touch.target.dispatchEvent(simulatedEvent);
-    //     event.preventDefault();
-    // }
-    //
-    // function init()
-    // {
-    //     document.addEventListener("touchstart", touchHandler, true);
-    //     document.addEventListener("touchmove", touchHandler, true);
-    //     document.addEventListener("touchend", touchHandler, true);
-    //     document.addEventListener("touchcancel", touchHandler, true);
-    // }
-    //
-    // // $(function(){
-    // $(document).ready(function() {
-    //     init();
-    // });
+    function touchHandler(event)
+    {
+        var touch = event.changedTouches[0];
+
+        var simulatedEvent = document.createEvent("MouseEvent");
+        simulatedEvent.initMouseEvent({
+                touchstart: "mousedown",
+                touchmove: "mousemove",
+                touchend: "mouseup"
+            }[event.type], true, true, window, 1,
+            touch.screenX, touch.screenY,
+            touch.clientX, touch.clientY, false,
+            false, false, false, 0, null);
+
+        touch.target.dispatchEvent(simulatedEvent);
+        event.preventDefault();
+    }
+
+    function init()
+    {
+        document.addEventListener("touchstart", touchHandler, true);
+        document.addEventListener("touchmove", touchHandler, true);
+        document.addEventListener("touchend", touchHandler, true);
+        document.addEventListener("touchcancel", touchHandler, true);
+    }
+
+    // $(function(){
+    $(document).ready(function() {
+        init();
+    });
     $(function(){
         $("#scroll-dish").sortable({
             stop: function(){
