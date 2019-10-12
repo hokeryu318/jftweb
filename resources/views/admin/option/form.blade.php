c7112ad4f00c3570944ebbd75f8c7e1552ae80fd
jiuhejong <hokelucpy@hotmail.com>
2019-10-13 00:18:15 +0800

    1012

--- resources/views/admin/option/form.blade.php
+++ resources/views/admin/option/form.blade.php
@@ -192,7 +192,7 @@
                 </div>
                 <div class="col-1">
                     <button class="btnaddphoto3 mt-3 fs-23" type="button" onclick="onDelete(this)" data-id={{ $item->id }}>
-                        Remove
+                        DEL
                     </button>
                 </div>
                 <input type="file" class="file-image" id="file-image" name="prev-data[{{ $item->id }}][image]" style="display:none">
@@ -226,10 +226,10 @@
             </div>
             <div class="col-1">
                 <button class="btnaddphoto3 mt-3 fs-23" type="button" onclick="onDelete(this)" data-id=0>
-                    Remove
+                    DEL
                 </button>
             </div>
-            <input type="file" class="file-image" name="image" style="display:none">
+            <input type="file" class="file-image" id="file-image" name="image" style="display:none">
             <input type="hidden" class="stock-check-value option-stock">
         </div>
 
