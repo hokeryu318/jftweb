15bf700d09c86cf4b2659f7bcf1fe96fb4c517d1
jiuhejong <hokelucpy@hotmail.com>
2019-10-12 13:15:38 +0800

    1012

--- app/Http/Controllers/OptionController.php
+++ app/Http/Controllers/OptionController.php
@@ -118,7 +118,7 @@ class OptionController extends Controller
                     $item->stock = $p['stock'];
 
                     if(isset(request()->file('prev-data')[$id]['image']) != null) {
-                        $image = request()->file('prev-data')[$id]['image'];
+                        $image = request()->file('prev-data')[$id]['image'];dd($image);
                         $destinationPath = 'options';
                         $destinationFile = "item".time().".png";
                         $image->move($destinationPath, $destinationFile);
